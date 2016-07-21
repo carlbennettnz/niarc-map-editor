b408df40bf41ae0acd6a872f9c968613082c82eb
Carl Bennett <carlbennettnz@gmail.com>
2016-07-22 10:20:07 +1200

    Add 'NIARC' to app name

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'map-editor',
+    modulePrefix: 'niarc-map-editor',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',

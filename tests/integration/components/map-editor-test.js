aa4ac01fd19e6dcb2e5058c69eccf9e5fd3dc3e4
Carl Bennett <carlbennettnz@gmail.com>
2016-08-02 14:41:41 +1200

    Add failing tests for the map-editor component

--- tests/integration/components/map-editor-test.js
+++ tests/integration/components/map-editor-test.js
@@ -13,6 +13,7 @@ const {
 } = Ember;
 
 const componentWithAllArgs = hbs`{{map-editor
+  layers=layers
   lines=lines
   viewport=viewport
   add=addLine
@@ -24,6 +25,7 @@ const componentWithAllArgs = hbs`{{map-editor
 
 describeComponent('map-editor', 'Integration: MapEditorComponent', { integration: true }, function() {
   beforeEach(function() {
+    this.set('layers', [{ name: 'map', isVisible: true, isSelected: true }]);
     this.set('lines', []);
     this.set('viewport', { scrollX: 0, scrollY: 0, zoom: 1 });
     this.set('addLine', line => this.get('lines').pushObject(line));
@@ -41,8 +43,8 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
 
     it('renders provided lines', function() {
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 20 } },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map' },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.render(componentWithAllArgs);
@@ -65,14 +67,33 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
       expect(lines[1].attr('y2')).to.equal('100');
     });
 
+    it('hides lines when layers are hidden', function() {
+      this.get('layers').pushObject({
+        name: 'path',
+        isVisible: true,
+        isSelected: false
+      });
+
+      this.set('lines', [
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map' },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'path' }
+      ]);
+
+      this.render(componentWithAllArgs);
+      expect(this.$('g.wall')).to.have.length(2, 'both lines rendered');
+
+      this.set('layers.1.isVisible', false);
+      expect(this.$('g.wall')).to.have.length(1, 'one line rendered');
+    });
+
     it('scales and zooms lines, axes, and the grid', function() {
       this.set('viewport.scrollX', 20);
       this.set('viewport.scrollY', 40);
       this.set('viewport.zoom', 0.5);
 
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 20 } },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map' },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.render(componentWithAllArgs);
@@ -108,14 +129,7 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
 
   describe('Adding new lines', function() {
     it('adds a line when you click and drag', function() {
-      this.render(hbs`{{map-editor
-        lines=lines
-        add=addLine
-        select=selectLine
-        deselectAll=deselectAllLines
-        resize=resizeLine
-        remove=removeLines
-      }}`);
+      this.render(componentWithAllArgs);
 
       this.$('svg')
         .trigger(mouseDownAt(20, 40))
@@ -128,6 +142,7 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
         expect(wall.attr('y1')).to.equal('40', 'y1');
         expect(wall.attr('x2')).to.equal('40', 'x2');
         expect(wall.attr('y2')).to.equal('40', 'y2');
+        expect(this.get('lines.0.layer')).to.equal('map', 'line layer');
       });
     });
 
@@ -231,42 +246,38 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
   describe('Selections', function() {
     it('selects an existing line on mouseup', function(done) {
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 20 } },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map' },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.set('selectLine', () => done());
 
       this.render(componentWithAllArgs);
 
-      const mouseDown = mouseDownAt(20, 20);
-      this.$('svg').trigger(mouseDown);
-
-      const mouseUp = mouseUpAt(20, 20);
-      this.$('svg').trigger(mouseUp);
+      this.$('svg')
+        .trigger(mouseDownAt(20, 20))
+        .trigger(mouseUpAt(20, 20));
     });
 
     it('selects an existing diagonal line on mouseup', function(done) {
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 100 } },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 100 }, layer: 'map' },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.set('selectLine', () => done());
 
       this.render(componentWithAllArgs);
 
-      const mouseDown = mouseDownAt(50, 50);
-      this.$('svg').trigger(mouseDown);
-
-      const mouseUp = mouseUpAt(50, 50);
-      this.$('svg').trigger(mouseUp);
+      this.$('svg')
+        .trigger(mouseDownAt(50, 50))
+        .trigger(mouseUpAt(50, 50));
     });
 
     it('deselects everything on mousedown in empty area', function() {
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 100 }, isSelected: true },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 100 }, layer: 'map', isSelected: true },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.render(componentWithAllArgs);
@@ -278,10 +289,34 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
       });
     });
 
+    it('does not select lines on unselected layers', function() {
+      this.get('layers').pushObject({
+        name: 'path',
+        isVisible: true,
+        isSelected: false
+      });
+
+      this.set('lines', [
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map' },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'path' }
+      ]);
+
+      this.render(componentWithAllArgs);
+
+      this.$('svg')
+        .trigger(mouseDownAt(60, 40))
+        .trigger(mouseUpAt(60, 40));
+
+      return wait().then(() => {
+        const selectedLines = this.get('lines').filterBy('isSelected');
+        expect(selectedLines).to.have.length(0, 'none selected');
+      });
+    });
+
     it('shows handles on selected lines', function() {
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, isSelected: true },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map', isSelected: true },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.render(componentWithAllArgs);
@@ -293,8 +328,8 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
   describe('Resizing', function() {
     it('edits the selected line when its handle is dragged', function() {
       this.set('lines', [
-        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, isSelected: true },
-        { points: { x1: 60, y1: 40, x2: 60, y2: 100 } }
+        { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map', isSelected: true },
+        { points: { x1: 60, y1: 40, x2: 60, y2: 100 }, layer: 'map' }
       ]);
 
       this.render(componentWithAllArgs);
@@ -316,7 +351,7 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
 
   describe('Moving', function() {
     it('moves a line when it is clicked and dragged', function() {
-      this.set('lines', [ { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, isSelected: true } ]);
+      this.set('lines', [ { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map', isSelected: true } ]);
       this.render(componentWithAllArgs);
 
       this.$('svg')
@@ -336,7 +371,7 @@ describeComponent('map-editor', 'Integration: MapEditorComponent', { integration
     });
 
     it('moves the selected line when an arrow key is pressed', function() {
-      this.set('lines', [ { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, isSelected: true } ]);
+      this.set('lines', [ { points: { x1: 20, y1: 20, x2: 100, y2: 20 }, layer: 'map', isSelected: true } ]);
       this.render(componentWithAllArgs);
 
       const codes = [ 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown' ];

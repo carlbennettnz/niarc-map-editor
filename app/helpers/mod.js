6ffdc02d1eb97824977e967f26438b4255a6c5bd
Megan Liang <meganliang15@hotmail.com>
2016-09-20 17:45:59 +1200

    updated mod class

--- app/helpers/mod.js
+++ app/helpers/mod.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-export function mod(params) {
-  return params[0]%10 === 0;
+export function mod([ number ] = []) {
+  return number % 10 === 0;
 }
 
 export default Ember.Helper.helper(mod);
